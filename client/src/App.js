ffa934566d6ccfd2dfd083d45d60d7ae4f6c3193
Samuel Adewole <samueladewole15@gmail.com>
2019-12-19 13:23:21 +0100

    init client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routes from './components/routes/Routes';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment className='App'>
+        <Switch>
+          <Navbar />
+          {/* <Route component={Routes} /> */}
+        </Switch>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/routes/Routes.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+export const Routers = () => {
+  return <Switch></Switch>;
+};

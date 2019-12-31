ccac32d8693affb740f0f213325d50a80850c51e
Samuel Adewole <samueladewole15@gmail.com>
2019-12-31 11:21:45 +0100

    clean up bugs

--- client/src/components/pages/ShoppingHistory.js
+++ client/src/components/pages/ShoppingHistory.js
@@ -1,106 +1,184 @@
-import React, {useState, Fragment} from 'react'
-import { Table } from 'antd';
+import React, {
+  useState,
+  Fragment,
+  useEffect
+} from 'react'
+import {
+  fetchUserOrderHistory
+} from '../../actions/orderAction'
+import {
+  Table
+} from 'antd';
+import {
+  useDispatch,
+  useSelector
+} from 'react-redux'
+import {
+  Table
+} from 'antd';
 
-const columns = [
-  {
-    title: 'Id',
+const columns = [{
+    title: 'FoodId',
     dataIndex: 'id',
   },
   {
-    title: 'Food Item',
-    dataIndex: 'item',
+    title: 'Food Name',
+    dataIndex: 'name',
   },
   {
-    title: 'Status',
-    dataIndex: 'status',
+    title: 'Email',
+    dataIndex: 'email',
   },
   {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <span>
-        <a>View details
-            <i className="far fa-arrow-square-down fa-2x text-white"></i>
-        </a>
-        {alert(3)}
-      </span>
-    ),
+    title: 'Quantity',
+    dataIndex: 'quantity',
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+  },
+  {
+    title: 'Unit price',
+    dataIndex: 'price',
+  },
+  {
+    title: 'Payment',
+    dataIndex: 'payment',
+  },
+  {
+    title: 'Shipping Address',
+    dataIndex: 'address',
+  },
+  {
+    title: 'Date',
+    dataIndex: 'date',
   },
 ];
 
 const data = [];
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < data.length; i++) {
   data.push({
     key: i,
     id: `Edward King ${i}`,
-    item: "Rice and Beans",
-    status: `London, Park Lane no. ${i}`,
+    name: 32,
+    email: `London, Park Lane no. ${i}`,
+    quantity: `London, Park Lane no. ${i}`,
+    amount: `London, Park Lane no. ${i}`,
+    price: `London, Park Lane no. ${i}`,
+    payment: `London, Park Lane no. ${i}`,
+    date: `London, Park Lane no. ${i}`,
   });
 }
 
-const ShoppingHistory =()=> {
-  const [selectedRowKeys, setSelectedRowKeys]=useState([]) // Check here to configure the default column
+{
+  "id": "218e5fae-083a-4308-a588-dc6151901da7",
+  "quantity": 3,
+  "amount": 2700,
+  "email": null,
+  "address": null,
+  "orderedDate": null,
+  "phone": null,
+  "status": "completed",
+  "payment": "pending",
+  "createdAt": "2019-12-29T12:42:09.693Z",
+  "updatedAt": "2019-12-30T13:01:27.133Z",
+  "user_id": "81f911ca-13ad-4b10-8eb7-0492dab5ffeb",
+  "menu_id": "15fa1536-de8d-4ed6-989b-5a298083ba19",
+  "food": {
+    "id": "15fa1536-de8d-4ed6-989b-5a298083ba19",
+    "name": "Beans and Ogbona",
+    "price": 900,
+    "image": "https://res.cloudinary.com/dovgnj0v4/image/upload/v1577780862/yqy1b4rhdxmnxtgnevvf.jpg",
+    "createdAt": "2019-12-22T23:17:37.830Z",
+    "updatedAt": "2019-12-31T08:27:55.843Z"
+  },
+  "user": {
+    "id": "81f911ca-13ad-4b10-8eb7-0492dab5ffeb",
+    "name": "Admin",
+    "email": "admin@crispymunch.com",
+    "role": "ADMIN",
+    "createdAt": "2019-12-26T23:44:10.486Z",
+    "updatedAt": "2019-12-26T23:44:10.486Z"
+  }
+}
 
+const ShoppingHistory = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]) // Check here to configure the default column
+  const dispatch = useDispatch()
+  const {
+    order
+  } = useSelector(state => {
+    return {
+      order: state.order
+    }
+  })
   const onSelectChange = selectedRowKeys => {
     console.log('selectedRowKeys changed: ', selectedRowKeys);
-    setSelectedRowKeys(selectedRowKeys);
+    this.setState({
+      selectedRowKeys
+    });
   };
 
-const rowSelection = {
+
+  const rowSelection = {
     selectedRowKeys,
     onChange: onSelectChange,
     hideDefaultSelections: true,
-    selections: [
-    {
+    selections: [{
         key: 'all-data',
         text: 'Select All Data',
         onSelect: () => {
-        setSelectedRowKeys([...Array(46).keys()]);
+          setSelectedRowKeys({
+            selectedRowKeys: [...Array(data.length).keys()], // 0...45
+          });
         },
-    },
-    {
+      },
+      {
         key: 'odd',
         text: 'Select Odd Row',
         onSelect: changableRowKeys => {
-        let newSelectedRowKeys = [];
-        newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changableRowKeys.filter((key, index) => {
             if (index % 2 !== 0) {
-            return false;
+              return false;
             }
             return true;
-        });
-        setSelectedRowKeys(newSelectedRowKeys);
+          });
+          this.setState({
+            selectedRowKeys: newSelectedRowKeys
+          });
         },
-    },
-    {
+      },
+      {
         key: 'even',
         text: 'Select Even Row',
         onSelect: changableRowKeys => {
-        let newSelectedRowKeys = [];
-        newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changableRowKeys.filter((key, index) => {
             if (index % 2 !== 0) {
-            return true;
+              return true;
             }
             return false;
-        });
-        setSelectedRowKeys(newSelectedRowKeys);
+          });
+          this.setState({
+            selectedRowKeys: newSelectedRowKeys
+          });
         },
-    },
+      },
     ],
-};
-return (
-    <Fragment>
-    <div className="mt-5 p-4 shopping_container">
-        <div className="menu-bg"></div>
-        <h1>Order History</h1>
-        <section className="bg-secondary text-white ">
-            <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
-        </section>
-    </div>  
-    </Fragment>    
-)
+  };
+  return <Table rowSelection = {
+    rowSelection
+  }
+  columns = {
+    columns
+  }
+  dataSource = {
+    data
+  }
+  />;
 }
 
 
-export default ShoppingHistory
 
+export default ShoppingHistory

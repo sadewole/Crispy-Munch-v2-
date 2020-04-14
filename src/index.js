cb659415366e818143bfe330312baed794976083
sadewole <samueladewole15@gmail.com>
2020-04-14 08:58:02 +0100

    prepare static file for prod

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import express from 'express';
+import path from 'path';
 import logger from 'morgan';
-import user from './routes/api/userRoute'
+import user from './routes/api/userRoute';
 import orderRoute from './routes/api/orderRoute';
 import menuRoute from './routes/api/menuRoute';
-import db from './db'
-import 'regenerator-runtime/runtime'
-import 'dotenv/config'
+import db from './db';
+import 'regenerator-runtime/runtime';
+import 'dotenv/config';
 
 const app = express();
 // middlewares
 app.use(logger());
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: false
-    })
+  express.urlencoded({
+    extended: false
+  })
 );
 
 // routes
@@ -22,16 +23,13 @@ app.use('/api/v1', user);
 app.use('/api/v1', menuRoute);
 app.use('/api/v1', orderRoute);
 
-
-
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get(/.*/, function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
@@ -48,9 +46,12 @@ const PORT = process.env.PORT || 5000;
 // }
 
 // sync database
-db.sequelize.sync().then(() => {
+db.sequelize
+  .sync()
+  .then(() => {
     console.log('Connection has been established successfully.');
-}).then(() => {
+  })
+  .then(() => {
     // listen to server
     app.listen(PORT, '0.0.0.0', console.log(`Server running on ${PORT}`));
-})
+  });
